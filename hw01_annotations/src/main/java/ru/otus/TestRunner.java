b030fa15d19e26cc3da031b67653a67b123928e3
VTegneryadnov <tegslava@gmail.com>
2024-09-22 17:06:43 +0300

    Д/р 1 выполнена

---
+++ hw01_annotations/src/main/java/ru/otus/TestRunner.java
@@ -0,0 +1,157 @@
+package ru.otus;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+import ru.otus.annotations.*;
+import ru.otus.testexceptions.MethodTestFailedException;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class TestRunner {
+    private static int disabledMethod;
+    private static int methodTestPassed;
+    private static int methodTestFailed;
+    private static Class clazz;
+
+    public static void run(Class testSuiteClass) {
+        clazz = testSuiteClass;
+        if (classDisabledCheck()) return;
+        var beforeSuiteMethods = getAnnotatedMethods(BeforeSuite.class);
+        var afterSuiteMethods = getAnnotatedMethods(AfterSuite.class);
+        var testSuiteMethods = getAnnotatedMethods(Test.class);
+        classMarkupCheck(beforeSuiteMethods, afterSuiteMethods);
+        execAdditionalMethod(beforeSuiteMethods, "Test initialization error: ");
+        testingMethods(testSuiteMethods);
+        execAdditionalMethod(afterSuiteMethods, "Test finalization error: ");
+        showTestsStatistic();
+    }
+
+    /**
+     * Выполняет методы инициализации и финализации тестов
+     * Допускает выполнения приватных методов
+     * Если есть дополнительная аннотоция @ Disabled, метод не выполняется
+     *
+     * @param methods массив методов
+     */
+    private static void execAdditionalMethod(Method[] methods, String message) {
+        for (var m : methods) {
+            if (!m.isAnnotationPresent(Disabled.class)) {
+                try {
+                    if (m.trySetAccessible()) m.invoke(null);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    throw new RuntimeException(message + e.getMessage());
+                }
+            } else {
+                disabledMethod++;
+                System.out.println(m.getName() + "\t" + m.getAnnotation(Disabled.class).reason());
+            }
+        }
+    }
+
+    /**
+     * Выполняет публичные методы, помеченные @Test
+     * Порядок выполнения от большего @Test(priority к меньшему
+     * Если есть дополнительная аннотоция @ Disabled, метод не выполняется
+     *
+     * @param testSuiteMethods массив методов
+     */
+    private static void testingMethods(Method[] testSuiteMethods) {
+        Comparator<Method> comparator = Comparator.comparingInt(a -> -1 * a.getAnnotation(Test.class).priority());
+        for (var m : Arrays.stream(testSuiteMethods).sorted(comparator).toList()) {
+            if (!m.isAnnotationPresent(Disabled.class)) {
+                try {
+                    try {
+                        m.invoke(null);
+                        methodTestPassed++;
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new MethodTestFailedException(m, e.getMessage());
+                    }
+                } catch (MethodTestFailedException ef) {
+                    methodTestFailed++;
+                    System.out.println(ef.getMessage());
+                }
+            } else {
+                disabledMethod++;
+                System.out.println(m.getName() + "\t" + m.getAnnotation(Disabled.class).reason());
+            }
+        }
+    }
+
+    /**
+     * Проверяет, помечен ли класс аннтоацией @Disabled
+     *
+     * @return boolean
+     */
+    private static boolean classDisabledCheck() {
+        Optional<Disabled> annotationClassDisabled = Optional.ofNullable((Disabled) clazz.getAnnotation(Disabled.class));
+        if (annotationClassDisabled.isPresent()) {
+            var reason = annotationClassDisabled.get().reason();
+            System.out.println("Test class " + clazz.getSimpleName() + " " + reason);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Проверяет разметку класса аннотациями, согласно правилам:
+     * - @BeforeSuite может быть в классе максимум 1
+     * - @AfterSuite может быть в классе максимум 1
+     * Аннотации @Test и @BeforeSuite/@AfterSuite не могут встречаться на одном и том же методе
+     * Если класс с тестами криво размечен, бросает исключение
+     *
+     * @param beforeSuiteMethods массив метолов
+     * @param afterSuiteMethods  массив метолов
+     */
+    private static void classMarkupCheck(Method[] beforeSuiteMethods, Method[] afterSuiteMethods) {
+        if (beforeSuiteMethods.length > 1) {
+            throw new IllegalArgumentException("BeforeSuite annotations more than 1");
+        }
+        if (afterSuiteMethods.length > 1) {
+            throw new IllegalArgumentException("AfterSuite annotations more than 1");
+        }
+        if (existCrossAnnotatedMethods(Test.class, BeforeSuite.class) ||
+                existCrossAnnotatedMethods(Test.class, AfterSuite.class) ||
+                existCrossAnnotatedMethods(BeforeSuite.class, AfterSuite.class)) {
+            throw new IllegalArgumentException("Invalid annotation intersection");
+        }
+    }
+
+    /**
+     * Проверяет наличие у метода двух аннотаций
+     *
+     * @param annotation1 первая аннтотация
+     * @param annotation2 вторая аннотация
+     * @return booolean
+     */
+    private static boolean existCrossAnnotatedMethods(Class<? extends Annotation> annotation1, Class<? extends Annotation> annotation2) {
+        return Arrays.stream(clazz.getDeclaredMethods()).anyMatch(x -> x.isAnnotationPresent(annotation1) && x.isAnnotationPresent(annotation2));
+    }
+
+    /**
+     * Возвращает массив методов, с заданной аннтоацией
+     *
+     * @param annotation искомая аннтотация
+     * @return массив методов
+     */
+    private static Method[] getAnnotatedMethods(Class<? extends Annotation> annotation) {
+        return Arrays.stream(clazz.getDeclaredMethods())
+                .filter(x -> x.isAnnotationPresent(annotation))
+                .toArray(Method[]::new);
+    }
+
+    /**
+     * Выводит статистику тестирования
+     */
+    public static void showTestsStatistic() {
+        System.out.printf("""
+
+                Method testing statistics for %s
+                  - tests passed %d;\r
+                  - tests failed %d;
+                  - tests disabled %d;
+
+                """, clazz.getName(), methodTestPassed, methodTestFailed, disabledMethod);
+    }
+}
---
+++ hw01_annotations/src/main/java/ru/otus/tests/TestSuiteDisabled.java
@@ -0,0 +1,55 @@
+package ru.otus.tests;
+
+import ru.otus.annotations.AfterSuite;
+import ru.otus.annotations.BeforeSuite;
+import ru.otus.annotations.Disabled;
+import ru.otus.annotations.Test;
+
+@Disabled
+public class TestSuiteDisabled {
+    @BeforeSuite
+    public static void init() {
+        System.out.println("init");
+    }
+
+    @Test(priority = 1)
+    public static void test1() {
+        System.out.println("test1\tpriority = 1");
+    }
+
+    @Test(priority = 6)
+    public static void test2() {
+        System.out.println("test2\tpriority = 6");
+    }
+
+    @Test
+    public static void test3() {
+        System.out.println("test3\tpriority = 5");
+    }
+
+    @Test(priority = 6)
+    public static void test4() {
+        System.out.println("test4\tpriority = 6");
+    }
+
+    @Test(priority = 10)
+    public static void test5() {
+        System.out.println("test5\tpriority = 10");
+    }
+
+    @Test(priority = 3)
+    public static void test6() {
+        System.out.println("test6\tpriority = 3");
+    }
+
+    @Disabled
+    @Test(priority = 4)
+    public static void test7() {
+        System.out.println("test7\tpriority = 4");
+    }
+
+    @AfterSuite
+    public static void test10() {
+        System.out.println("finished");
+    }
+}

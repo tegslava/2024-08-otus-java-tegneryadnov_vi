a1c659d4024117f0deabfc78fcab796f594f399b
VTegneryadnov <tegslava@gmail.com>
2024-10-03 13:58:06 +0300

    Д/р 3 version = 1.0.0-SNAPSHOT несущественные изменения

--- hw03-stream-api/src/main/java/ru/otus/task/Task.java
+++ hw03-stream-api/src/main/java/ru/otus/task/Task.java
@@ -2,13 +2,15 @@ package ru.otus.task;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Builder
+@ToString
 public class Task {
     @Getter
     private long id;
     @Getter
     private String name;
     @Getter
-    private TaskStatus taskStatus = TaskStatus.NEW;
+    private TaskStatus taskStatus;
 }

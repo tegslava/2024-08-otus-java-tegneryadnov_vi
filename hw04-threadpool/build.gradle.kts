31072688beeb4c00b6157410ce3aeb876b112daa
tegslava <tegslava@gmail.com>
2024-10-15 22:08:19 +0300

    Д/р 4 в работе, добавил сторонние учебные классы

--- hw04-threadpool/build.gradle.kts
+++ hw04-threadpool/build.gradle.kts
@@ -5,6 +5,12 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
+tasks.withType<JavaCompile> {
+    //options.encoding = "UTF-8"
+    options.encoding = "windows-1251"
+    options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+}
+
 tasks.test {
     useJUnitPlatform()
 }
--- hw04-threadpool/src/main/java/ru/otus/threadpool/MyThreadPool.java
+++ hw04-threadpool/src/main/java/ru/otus/threadpool/MyThreadPool.java
@@ -1,5 +1,40 @@
 package ru.otus.threadpool;
 
-public class MyThreadPool {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyThreadPool {
+    private static final Logger logger = LoggerFactory.getLogger(MyThreadPool.class);
+    private final int MIN_POOL_SIZE = 0;
+    private final int MAX_POOL_SIZE = 100;
+    private final List<Thread> threadPool;
+    private final int threadPoolSize;
+
+    public MyThreadPool(int threadPoolSize) {
+        this.threadPoolSize = threadPoolSize;
+        if (threadPoolSize > MIN_POOL_SIZE && threadPoolSize <= MAX_POOL_SIZE) {
+            threadPool = getFillingThreadList();
+        } else {
+            throw new IllegalArgumentException("Invalid value threadPoolSize " + threadPoolSize);
+        }
+    }
+
+    private ArrayList<Thread> getFillingThreadList() {
+        var threadList = new ArrayList<Thread>(threadPoolSize);
+        for (int i = 0; i < threadPoolSize; i++) {
+            threadList.add(new Thread());
+        }
+
+
+        return threadList;
+    }
+
+    public static void main(String[] args) {
+        MyThreadPool myThreadlogger = new MyThreadPool(5);
+        //logger.info("{}", myThreadlogger.threadList.size());
+        //myThreadlogger.threadList.forEach(x -> logger.info("{}", x.getName() + " " + x.threadId()));
+    }
 }

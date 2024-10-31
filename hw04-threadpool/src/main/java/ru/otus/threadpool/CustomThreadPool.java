70b08251e4eaacf0f46b6e8e759c5202890576a0
tegslava <tegslava@gmail.com>
2024-10-31 14:05:42 +0300

    Д/р 4 выполнил

--- hw04-threadpool/src/main/java/ru/otus/threadpool/CustomThreadPool.java
+++ hw04-threadpool/src/main/java/ru/otus/threadpool/CustomThreadPool.java
@@ -1,135 +1,109 @@
 package ru.otus.threadpool;
 
+import java.util.LinkedList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//import ru.otus.workerthread.WokerThread;
-
-//import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-//import java.util.concurrent.Executors;
-
 
 public class CustomThreadPool {
     private static final Logger logger = LoggerFactory.getLogger(CustomThreadPool.class);
     private static final int MIN_THREAD_POOL_SIZE = 1;
     private static final int MAX_THREAD_POOL_SIZE = 100;
     private final Thread[] arrThreads;
-    private static final BlockingQueue queue = new CustomThreadPool.BlockingQueue();
-    private volatile static boolean running = true;
-    private volatile static boolean terminated;
+    private final BlockingQueue queue;
+    private volatile boolean running = true;
+    private final int threadPoolSize;
 
     public CustomThreadPool(int threadPoolSize) {
+        this.threadPoolSize = threadPoolSize;
         if (threadPoolSize < MIN_THREAD_POOL_SIZE || threadPoolSize > MAX_THREAD_POOL_SIZE) {
             throw new IllegalArgumentException("Invalid value threadPoolSize " + threadPoolSize);
         }
-        arrThreads = new Thread[threadPoolSize];
-        initialize(threadPoolSize);
-        //ExecutorService pool = Executors.newFixedThreadPool(10);
+        queue = new CustomThreadPool.BlockingQueue();
+        arrThreads = initialize();
     }
 
-    private void initialize(int threadNum) {
-        for (int i = 0; i < threadNum; i++) {
-            int idx = i;
-            Thread worker = new Thread(() -> {
-                while (running) {
-                    Runnable task = queue.get();
-                    task.run();
-                }
-            });
-            worker.setName("Поток " + idx);
-            arrThreads[i] = worker;
-            worker.start();
+    public final void showThreadsStates() {
+        for (var t : arrThreads) {
+            logger.debug("{} {}", t.getName(), t.getState());
         }
     }
 
-    public void execute(Runnable task) {
-        queue.put(task);
+    private Thread[] initialize() {
+        Thread[] arr = new Thread[threadPoolSize];
+        for (int i = 0; i < threadPoolSize; i++) {
+            Thread worker = new Thread(() -> {
+                while (running) {
+                    Runnable task = queue.get();
+                    if (task != null) task.run();
+                }
+            });
+            worker.setName("Поток " + i);
+            arr[i] = worker;
+            worker.start();
+        }
+        return arr;
     }
 
-    private final static class BlockingQueue {
+    public void execute(Runnable task) {
+        if (running) {
+            queue.put(task);
+        }
+    }
+
+    private static final class BlockingQueue {
         LinkedList<Runnable> tasks = new LinkedList<>();
 
         synchronized Runnable get() {
-            while (tasks.isEmpty()) {
+            while (tasks.isEmpty() && !Thread.currentThread().isInterrupted()) {
                 try {
                     wait();
                 } catch (InterruptedException e) {
-                    //logger.error(" isEmpty {}", e.getMessage());
+                    Thread.currentThread().interrupt();
+                    return null;
                 }
             }
-            Runnable task = tasks.poll();
-            return task;
+            return tasks.poll();
         }
 
         synchronized void put(Runnable task) {
             tasks.add(task);
-            notify();
         }
     }
 
-    static Runnable getTask(int taskNum, CountDownLatch latch) {
+    static Runnable getTask(int taskNum) {
         final int task_num = taskNum;
-        return new Runnable() {
-            @Override
-            public void run() {
-                //logger.debug("Task {} started", taskNum);
-                try {
-                    Thread.sleep(1000);
-                    //latch.countDown();
-                } catch (InterruptedException e) {
-                    logger.info(
-                            "Trying to stop {} interrupt = {}", taskNum,
-                            Thread.currentThread().isInterrupted());
-                    // восстанавливаем флаг isInterrupted()
-                    Thread.currentThread().interrupt();
-                }
-                logger.debug("Task {} finished", taskNum);
+        return () -> {
+            try {
+                Thread.sleep(100);
+                logger.info("Task {} finished", task_num);
+            } catch (InterruptedException e) {
+                logger.debug("Trying to stop {} Task {}", Thread.currentThread().getName(), task_num);
+                Thread.currentThread().interrupt();
             }
         };
     }
 
     public void shutdown() {
-        terminated = true;
         running = false;
-/*
         for (Thread thread : arrThreads) {
-            //thread.notify();
-            thread.interrupt();
+            if (!thread.isInterrupted()) thread.interrupt();
         }
-*/
-
+        logger.debug("shutdown()");
     }
 
-    public boolean isTerminated() {
-        return terminated;
-    }
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        int threadNum = 10;
         int taskNum = 100;
         int treshHold = 5;
-        CustomThreadPool customThreadPool = new CustomThreadPool(10);
-        //CountDownLatch latch = new CountDownLatch(treshHold);
+        CustomThreadPool pool = new CustomThreadPool(threadNum);
         for (int i = 0; i < taskNum; i++) {
-            try {
-                customThreadPool.execute(getTask(i, null));
-                if(i== treshHold) {customThreadPool.shutdown();}
-                //latch.countDown();
-            } catch (Exception e) {
-                logger.error("for ... {}", e.getMessage());
-                throw new RuntimeException(e);
+            if (i == treshHold) {
+                Thread.sleep(1000);
+                pool.shutdown();
             }
+            pool.execute(getTask(i));
         }
-        try {
-            //latch.await();
-            //customThreadPool.shutdown();
-            Thread.sleep(2000);
-            logger.info("DONE");
-        } catch (InterruptedException e) {
-            logger.error("fff {}", e.getMessage());
-            throw new RuntimeException(e);
-        }
+        Thread.sleep(1);
+        pool.showThreadsStates();
     }
 }

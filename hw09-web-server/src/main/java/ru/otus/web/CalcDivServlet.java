5021d15251b06137eb051d56fb2e9e06503d3d69
tegslava <tegslava@gmail.com>
2024-12-09 00:48:14 +0300

    Д/з 9 выполнил

---
+++ hw09-web-server/src/main/java/ru/otus/web/CalcDivServlet.java
@@ -0,0 +1,27 @@
+package ru.otus.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@WebServlet(name = "CalcDivServlet", urlPatterns = "/div")
+public class CalcDivServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(CalcDivServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, NumberFormatException {
+        PrintWriter out = resp.getWriter();
+        resp.setContentType("text/html");
+        int a = Integer.parseInt(req.getParameter("a"));
+        int b = Integer.parseInt(req.getParameter("b"));
+        String result = b != 0 ? String.valueOf(a / b) : "ERROR division by 0";
+        out.printf("<html><body><h1>%d / %d = %s</h1></body></html>", a, b, result);
+        logger.debug("servlet executed");
+        out.close();
+    }
+}
---
+++ hw09-web-server/src/main/java/ru/otus/web/FilterServlets.java
@@ -0,0 +1,37 @@
+package ru.otus.web;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("java:S1186")
+@WebFilter(urlPatterns = {"/add", "/sub", "/multiply", "/div"})
+public class FilterServlets implements Filter {
+    private static final Logger logger = LoggerFactory.getLogger(FilterServlets.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        StringBuilder sb = new StringBuilder();
+        Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            String value = httpServletRequest.getParameter(name);
+            sb.append(String.format("%s = %s; ", name, value));
+        }
+        logger.info("urlPattern: {}; params: {}", httpServletRequest.getServletPath(), sb);
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void destroy() {}
+}
